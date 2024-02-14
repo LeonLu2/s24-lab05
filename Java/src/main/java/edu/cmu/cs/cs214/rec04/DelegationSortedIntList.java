f6d88c913a5b28c2427918baf25513475706704f
suhanlu <suhanl@andrew.cmu.edu>
2024-02-13 22:04:14 -0500

    1. add attributes and methods to DelegationSortedIntList class to implement IntergerList

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,60 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList{
     // Write your implementation below with API documentation
+    private int totalAdded;
+    private SortedIntList list;
 
+    // constructor
+    public DelegationSortedIntList() {
+        list = new SortedIntList();
+        totalAdded = 0;
+    }
+
+    // implement get method
+    public int get(int index) {
+        return list.get(index);
+    }
+
+    // implement size method
+    public int size() {
+        return list.size();
+    }
+
+    // implement remove method
+    public boolean remove(int num) {
+        return list.remove(num);
+    }
+
+    // implement removeAll method
+    public boolean removeAll(IntegerList list) {
+        return this.list.removeAll(list);
+    }
+
+    // getter for totalAdded
+    public int getTotalAdded() {
+        return totalAdded;
+    }
+
+    // implement add method, increment totalAdded
+    public boolean add(int num) {
+        boolean success = list.add(num);
+        if (success) {
+            totalAdded++;
+        }
+        return success;
+    }
+
+    // implement addAll method, increment totalAdded
+    public boolean addAll(IntegerList list) {
+        boolean success = false;
+        for (int i = 0; i < list.size(); i++) {
+            success |= list.add(list.get(i));
+            if (success) {
+                totalAdded++;
+            }
+        }
+        return success;
+    }
 }
